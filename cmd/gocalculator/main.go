4c1e7692c018fa8e9e364457d756f8c43058bc74
Sanjay R <sanjayrachwani@yahoo.com>
2025-01-28 21:55:27 +0100

    env variable loading.

--- cmd/gocalculator/main.go
+++ cmd/gocalculator/main.go
@@ -2,23 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
 
 	"github.com/Sanjay-R/gocalculator/internal/handlers" //import package from my own module
 	"github.com/go-chi/chi"                              //flexible and easy to use package for web dev
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	slog.SetDefault(logger) //so anywhere I do slog.info it uses the logger just created
 
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	var r *chi.Mux = chi.NewRouter()
 	handlers.ApiHandler(r)
 
-	port := 3000
-	addr := fmt.Sprintf(":%d", port)
+	port := os.Getenv("PORT")
+	addr := fmt.Sprintf(":%v", port)
 	fmt.Println("Starting the GO stateless Calculator backend service at " + addr)
 
 	err := http.ListenAndServe(addr, r) //start server with http package
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Sanjay-R/gocalculator
 go 1.23.3
 
 require github.com/go-chi/chi v1.5.5
+
+require github.com/joho/godotenv v1.5.1
